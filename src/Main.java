bd00c4ed416e29f695ae1e515f4dc4781ace1740
Jef7 <jecam94@hotmail.com>
2016-05-26 16:41:32 -0600

    estructura MVC

---
+++ src/Main.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Jeffrey on 26/5/2016.
+ */
+public class Main {
+}
---
+++ src/view/console/PrestamosFrame.java
@@ -0,0 +1,7 @@
+package view.console;
+
+/**
+ * Created by Jeffrey on 26/5/2016.
+ */
+public class PrestamosFrame {
+}
