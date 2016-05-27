c1b72e8e3e127ac3cf26655ad4f0cf6f577e8051
Jef7 <jecam94@hotmail.com>
2016-05-26 21:38:15 -0600

    GUI Listo

--- src/controller/PrestamosFrameController.java
+++ src/controller/PrestamosFrameController.java
@@ -7,18 +7,49 @@ import javax.swing.*;
  */
 public class PrestamosFrameController {
     private PrestamosFrame prestamosFrame; //referencia de la vista
-
     private JPanel prestamosPanel; //panel es la base
+
+    //paneles
+    private JPanel mainPanel; //panel es la base
+    private JPanel registroPanel; //panel hijo
+    private JPanel resultadosPanel; //panel hijo
+
+    //REGISTRO
+    //labels
     private JLabel lbl_nombre;
     private JLabel lbl_monto;
     private JLabel lbl_plazo;
+    private JLabel lbl_interes;
     private JLabel lbl_sistemaAmortizacion;
     private JLabel lbl_moneda;
+    //text fields
     private JTextField txt_nombre;
     private JTextField txt_monto;
     private JTextField txt_plazo;
+    private JTextField txt_interes;
+    //combo box
     private JComboBox comBox_sistemaAmortizacion;
     private JComboBox comBox_moneda;
+    //botones
+    private JButton btn_registrar;
+
+    //RESULTADOS
+    //labels
+    private JLabel lbl_fechaHora;
+    private JLabel lbl_tipoCambio;
+    private JLabel lbl_infoCliente;
+    private JLabel lbl_tabla;
+    //text fields
+    private JTextField txt_fechaHora;
+    private JTextField txt_tipoCambio;
+    private JTextArea text_info;
+    //tabla
+    private JTable table_amortizacion;
+    //botones
+    private JButton btn_ok;
+    //fondo
+    private JPanel fondoPanel;
+    private JLabel lbl_fondo;
 
     //constructor
     public PrestamosFrameController() {
@@ -28,6 +59,8 @@ public class PrestamosFrameController {
     //muestra la ventana
     public void showPrestamosFrame(){
         prestamosFrame.setVisible(true);
+
+        prestamosFrame.setExtendedState(JFrame.MAXIMIZED_BOTH);//para poner la venta en FULLSCREEN
     }
 
     //carga los componentes del frame
